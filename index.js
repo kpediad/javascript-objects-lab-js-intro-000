b3c5cbda731aa53cd84c24c7767960e3be7f71e8
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-23 13:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  var cloneObject = object
+  delete cloneObject[key]
+  return cloneObject
+}
